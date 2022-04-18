d10923526af14a5642d3bbb1e98122d922bd9683
Bloodcloak <59000966+bloodcloak@users.noreply.github.com>
2022-04-17 22:19:13 -0400

    add role app endpoints

--- webhook_forwarder.py
+++ webhook_forwarder.py
@@ -21,16 +21,27 @@ reportsURL = environ.get('REPORTS_WEBHOOK')
 contactsURL = environ.get('CONTACTS_WEBHOOK')
 wikiURL = environ.get('WIKI_WEBHOOK')
 
+modelsURL = environ.get('MODELS_WEBHOOK')
+roleAppsURL = environ.get('ROLEAPPS_WEBHOOK')
+
 if reportsURL is None:
-    logging.error("Must Define REPORTS_WEBHOOK")
+    logging.error("Must Define REPORTS_WEBHOOK: ", reportsURL)
     exit(1)
 
 if contactsURL is None:
-    logging.error("Must Define CONTACTS_WEBHOOK")
+    logging.error("Must Define CONTACTS_WEBHOOK: ", contactsURL)
     exit(1)
 
 if wikiURL is None:
-    logging.error("Must Define WIKI_WEBHOOK")
+    logging.error("Must Define WIKI_WEBHOOK:", wikiURL)
+    exit(1)
+
+if modelsURL is None:
+    logging.error("Must Define MODELS_WEBHOOK:", modelsURL)
+    exit(1)
+
+if roleAppsURL is None:
+    logging.error("Must Define ROLEAPPS_WEBHOOK:", roleAppsURL)
     exit(1)
 
 subdivideRegex = r"[\s\S]{1,1024}"
@@ -260,6 +271,121 @@ async def wiki():
             embed.add_field(name='Message', value=articlebody.get_text(), inline=False)
 
         await wikiHook.send(embed=embed)
+    resp = jsonify(success=True)
+    return resp
+
+@application.route('/models', methods=['POST'])
+async def models():
+    # validate inbound webhook
+    header_signature = request.headers.get('X-Hub-Signature')
+    verifyHeaders(header_signature)
+
+    rDict = request.json
+    async with aiohttp.ClientSession() as session:
+        postHook = Webhook.from_url(modelsURL, adapter = AsyncWebhookAdapter(session))
+
+        embedTitle = '3D Artist Application | Ticket #' + str(rDict['ticket']['id'])
+        ticketURL = baseTicketURL + str(rDict['ticket']['id'])
+        embed = Embed(title=embedTitle,url=ticketURL)
+
+        embed.add_field(name='User', value=rDict['ticket']['customer']['firstname'], inline=False)
+        embed.add_field(name='User ID', value=rDict['ticket']['customer']['login'], inline=False)
+        embed.add_field(name='Ticket Title', value=rDict['ticket']['title'], inline=False)
+        
+        alt_contact = rDict['ticket']['aalternative_contact']
+        if alt_contact:
+            embed.add_field(name='Alternative Contact', value=alt_contact, inline=False)
+
+        modelsaber_username = rDict['ticket']['modelsaber_username']
+        if modelsaber_username:
+            embed.add_field(name='Modelsaber Username', value=modelsaber_username, inline=False)
+
+        for linkNumber in range(1,6):
+            current_link = 'submission_link' + str(linkNumber)
+            fieldValue = rDict['ticket'][current_link]
+            if fieldValue:
+                fieldName = 'Link ' + str(linkNumber)
+                embed.add_field(name=fieldName, value=fieldValue, inline=False)
+
+        system_attachments = rDict['article']['attachments']
+        if system_attachments:
+            embed.add_field(name="Additional Files", value="Yes - See Ticket", inline=False)
+
+        articlebody = BeautifulSoup(rDict['article']['body'])
+        try:
+            parts = findall(subdivideRegex, articlebody.get_text())
+            for part in parts:
+                embed.add_field(name='Message', value=part, inline=False)
+        except:
+            embed.add_field(name='Message', value=articlebody.get_text(), inline=False)
+
+        await postHook.send(embed=embed)
+    resp = jsonify(success=True)
+    return resp
+
+@application.route('/modapps', methods=['POST'])
+async def modapps():
+    # validate inbound webhook
+    header_signature = request.headers.get('X-Hub-Signature')
+    verifyHeaders(header_signature)
+
+    rDict = request.json
+    embedTitle = 'Moderator Role App | Ticket #' + str(rDict['ticket']['id'])
+    ticketURL = baseTicketURL + str(rDict['ticket']['id'])
+    embed = Embed(title=embedTitle,url=ticketURL)
+
+    return roleapphelper(rDict, embed)
+
+@application.route('/staffapps', methods=['POST'])
+async def staffapps():
+    # validate inbound webhook
+    header_signature = request.headers.get('X-Hub-Signature')
+    verifyHeaders(header_signature)
+
+    rDict = request.json
+    embedTitle = 'Staff Role App | Ticket #' + str(rDict['ticket']['id'])
+    ticketURL = baseTicketURL + str(rDict['ticket']['id'])
+    embed = Embed(title=embedTitle,url=ticketURL)
+
+    return roleapphelper(rDict, embed)
+    
+async def roleapphelper(rDict, embed):
+    async with aiohttp.ClientSession() as session:
+        postHook = Webhook.from_url(roleAppsURL, adapter = AsyncWebhookAdapter(session))
+
+        embed.add_field(name='User', value=rDict['ticket']['customer']['firstname'], inline=False)
+        embed.add_field(name='User ID', value=rDict['ticket']['customer']['login'], inline=False)
+        embed.add_field(name='Ticket Title', value=rDict['ticket']['title'], inline=False)
+        
+        alt_contact = rDict['ticket']['aalternative_contact']
+        if alt_contact:
+            embed.add_field(name='Alternative Contact', value=alt_contact, inline=False)
+
+        dateofbirth = rDict['ticket']['dateofbirth']
+        if dateofbirth:
+            embed.add_field(name='Date of Birth', value=dateofbirth, inline=False)
+
+        location = rDict['ticket']['location']
+        if location:
+            embed.add_field(name='What country (and state) and timezone are you based in?', value=location, inline=False)
+
+        twofa = rDict['ticket']['2fa_affirm']
+        if twofa:
+            embed.add_field(name='Do you have Two-Factor Authentication Enabled?', value=twofa, inline=False)
+
+        system_attachments = rDict['article']['attachments']
+        if system_attachments:
+            embed.add_field(name="Additional Files", value="Yes - See Ticket", inline=False)
+
+        articlebody = BeautifulSoup(rDict['article']['body'])
+        try:
+            parts = findall(subdivideRegex, articlebody.get_text())
+            for part in parts:
+                embed.add_field(name='Message', value=part, inline=False)
+        except:
+            embed.add_field(name='Message', value=articlebody.get_text(), inline=False)
+
+        await postHook.send(embed=embed)
     resp = jsonify(success=True)
     return resp
 
